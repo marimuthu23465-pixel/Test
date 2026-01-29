47c8afa5acdf7d964fd569583298075c52c694f9
marimuthu23465-pixel <marimuthu23465@gmail.com>
2026-01-29 05:38:34 -0800

    for HomeWork

---
+++ Food.java
@@ -0,0 +1,14 @@
+class Food {
+    public static void main(String[] args) {
+
+        String foodName = "Paneer Butter Masala";
+        boolean isVegetarian = true;
+        int calories = 400;
+        double price = 250.50;
+
+        System.out.println(foodName);
+        System.out.println(isVegetarian);
+        System.out.println(calories);
+        System.out.println(price);
+    }
+}
