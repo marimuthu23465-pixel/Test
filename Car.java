dffe50b755ec34783c77a4579f13718d3234f776
marimuthu23465-pixel <marimuthu23465@gmail.com>
2026-01-29 05:13:07 -0800

    for HomeWork

---
+++ Car.java
@@ -0,0 +1,16 @@
+class Car {
+    public static void main(String[] args) {
+
+        String carName = "Tesla Model S";
+        String fuelType = "Electric";
+        int mileage = 396;
+        double price = 85000.00;
+        String brand = "Tesla";
+
+        System.out.println(carName);
+        System.out.println(fuelType);
+        System.out.println(mileage);
+        System.out.println(price);
+        System.out.println(brand);
+    }
+}
